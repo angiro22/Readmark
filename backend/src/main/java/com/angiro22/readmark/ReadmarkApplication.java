fc1c711730d62daa1f949071525ca119712fd426
angiro22 <andreagirone2710@gmail.com>
2026-05-16 14:21:00 +0200

    Setting up spring boot

---
+++ backend/src/main/java/com/angiro22/readmark/ReadmarkApplication.java
@@ -0,0 +1,13 @@
+package com.angiro22.readmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadmarkApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/angiro22/readmark/ReadmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angiro22.readmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
